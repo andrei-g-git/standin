d221c28addc8ee99e717fd1c578c4709dd666ddf
unknown <andrei_g_git@protonmail.com>
2022-07-08 10:50:24 +0300

    add 49 nitter alts

--- js/domains.dev.js
+++ js/domains.dev.js
@@ -121,7 +121,7 @@ function getSupportedDomains() {
   }, {
     group: "twitter",
     domains: ["https://twitter.com", //nitter
-    "https://nitter.net"]
+    "https://nitter.net", "https://nitter.42l.fr", "https://nitter.pussthecat.org", "https://nitter.kavin.rocks", "https://nitter.unixfox.eu", "https://nitter.namazso.eu", "https://nitter.hu", "https://nitter.moomoo.me", "https://nitter.it", "https://twitter.censors.us", "https://nitter.grimneko.de", "https://nitter.ca", "https://twitter.076.ne.jp ", "https://nitter.fly.dev ", "https://notabird.site ", "https://nitter.weiler.rocks", "https://nitter.sethforprivacy.com", "https://nttr.stream", "https://nitter.cutelab.space", "https://nitter.nl", "https://nitter.mint.lgbt", "https://nitter.bus-hit.me", "https://nitter.esmailelbob.xyz", "https://tw.artemislena.eu", "https://de.nttr.stream", "https://nitter.winscloud.net", "https://nitter.tiekoetter.com", "https://nitter.spaceint.fr", "https://twtr.bch.bar", "https://nitter.privacy.com.de", "https://nitter.mastodon.pro", "https://nitter.notraxx.ch", "https://nitter.poast.org", "https://nitter.bird.froth.zone", "https://nitter.dcs0.hu", "https://twitter.dr460nf1r3.org", "https://twitter.beparanoid.de", "https://n.ramle.be", "https://nitter.cz", "https://nitter.privacydev.net", "https://tweet.lambda.dance", "https://nitter.ebnar.xyz", "https://nitter.kylrth.com", "https://nitter.oishi-ra.men", "https://nitter.foss.wtf", "https://nitter.priv.pw", "https://t.com.sb", "https://nt.vern.cc", "https://nitter.wef.lol", "https://nitter.tokhmi.xyz", "https://nitter.catalyst.sx"]
   }, {
     group: "reddit",
     domains: ["https://reddit.com", //reddit
